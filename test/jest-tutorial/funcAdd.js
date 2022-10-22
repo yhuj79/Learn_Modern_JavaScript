8f8b02d307ecb50ed873f1449bb57e6d8aabb5dd
yhuj79 <yu1085@naver.com>
2022-10-22 12:09:15 +0900

    jest test (funcAdd)

---
+++ test/jest-tutorial/funcAdd.js
@@ -0,0 +1,5 @@
+const funcAdd = {
+  add: (num1, num2) => num1 + num2,
+};
+
+module.exports = funcAdd;
---
+++ test/jest-tutorial/funcAdd.test.js
@@ -0,0 +1,13 @@
+const funcAdd = require("./funcAdd");
+
+test("1 => 1", () => {
+  expect(1).toBe(1);
+});
+
+test("2 + 3 = 5", () => {
+  expect(funcAdd.add(2, 3)).toBe(5);
+});
+
+test("3 + 3 ≠ 5", () => {
+  expect(funcAdd.add(3, 3)).not.toBe(5);
+});
