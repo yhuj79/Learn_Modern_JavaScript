ccfd279d29fdd1f4b19ab685d10f360f0ea39896
yhuj79 <yu1085@naver.com>
2022-10-23 12:02:59 +0900

    jest test (matchers 1)

--- test/jest-tutorial/funcAdd.js
+++ test/jest-tutorial/funcAdd.js
@@ -1,5 +1,6 @@
 const funcAdd = {
   add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => ({name, age}),
 };
 
 module.exports = funcAdd;
--- test/jest-tutorial/funcAdd.test.js
+++ test/jest-tutorial/funcAdd.test.js
@@ -11,3 +11,22 @@ test("2 + 3 = 5", () => {
 test("3 + 3 ≠ 5", () => {
   expect(funcAdd.add(3, 3)).not.toBe(5);
 });
+
+
+
+test("Make User Test", () => {
+  expect(funcAdd.makeUser("James", 33)).toEqual({ name: "James", age: 33 });
+});
+
+test("Make User Test", () => {
+  expect(funcAdd.makeUser("James", 33)).toStrictEqual({ name: "James", age: 33 });
+});
+
+// toBeNull
+// toBeFalsey
+// toBeTruthy
+// toBeLessThanOrEqual
+// toBeCloseTo
+// toMatch
+// toContain
+// toThrow
